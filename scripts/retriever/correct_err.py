4e55cf23c91a9e51bd028f8371b99a1cf6c8d049
Liang Shuailong <liangshuailong@gmail.com>
2018-02-18 23:45:06 +0800

    update script

--- scripts/retriever/correct_err.py
+++ scripts/retriever/correct_err.py
@@ -4,11 +4,6 @@
 Analyze err file created by eval_err.py
 created by Shuailong at 2018/2/10.
 
-Load file. Count total error cases.
-Input: case number (1-N)
-Output: question, answer, candidates list (title, score)
-Input: article title
-Output: article full text
 '''
 
 import argparse
@@ -17,19 +12,25 @@ import logging
 import json
 import sys
 import os
-from shutil import copyfile
 import pathlib
-
 from termcolor import colored
-
 from collections import Counter
+import readline
 
 from drqa import retriever
 
 
+def rlinput(prompt, prefill=''):
+    readline.set_startup_hook(lambda: readline.insert_text(prefill))
+    try:
+        return input(prompt)
+    finally:
+        readline.set_startup_hook()
+
+
 def print_case(d):
     question, answer, title, context, candidates = d['question'], d['answer'], d['title'], d['context'], d['candidates']
-    print('\n===============================================\n')
+    question = colored(question, color='yellow', attrs=['bold'])
     print(f'Question:\n{question}\n\nAnswer:\n{answer}\n')
     if title:
         print(f"Correct Title:\n{title}\n\nContext:\n")
@@ -37,7 +38,7 @@ def print_case(d):
         ans = [ans for ans in answer if ans in context][0]
         start = context.find(ans)
         end = start + len(ans)
-        print(context[:start] + colored(context[start: end], 'green', attrs=['bold']) + context[end:])
+        print(context[:start] + colored(context[start: end], color='green', attrs=['bold']) + context[end:])
     print('\nTop 5 docs:')
     table = prettytable.PrettyTable(
         ['Rank', 'Doc Id', 'Doc Score']
@@ -47,70 +48,47 @@ def print_case(d):
     print(table)
 
 
-def correct_cases(remained_cases, corr_file):
-    dirty = 0
+def correct_cases(cases, corr_file):
+    out_f = open(corr_file, 'a')
     reformulated = 0
-
-    for case_id, d in enumerate(remained_cases):
+    passed = 0
+    for case_id, d in enumerate(cases):
+        print('\n######################')
+        print(f'#### {case_id + 1:4} / {len(cases):4}  ####')
+        print('######################')
         print_case(d)
-        tag = input('Need reformulate: ')
-        if tag.lower() in ['1', 'y', 'yes']:
-            q = input('Reformulated question:\n>> ')
+        tag = rlinput('Need reformulate (y/n): ', prefill='n')
+        if tag.lower().strip() != 'n':
+            q = rlinput('Reformulated question:\n>> ', prefill=d['question'])
             d['sq'] = q.strip()
             reformulated += 1
-        d['dirty'] = 'Y'
-        dirty += 1
-        c = input('Continue (y/n): ')
-        if c == 'n':
+        passed += 1
+        out_f.write(json.dumps(d) + '\n')
+        out_f.flush()
+        c = rlinput('Continue (y/n): ', prefill='y')
+        if c.lower().strip() == 'n':
             break
-
-    logger.info(f'Reformulated {reformulated} cases out of {dirty}. Remains {len(remained_cases) - dirty} cases to explore.')
-
-    with open(corr_file, 'a') as f:
-        for c in remained_cases:
-            f.write(json.dumps(c) + '\n')
-        f.flush()
-    logger.info(f'Remaining {len(remained_cases)} cases saved back to {corr_file}.')
+    out_f.close()
+    logger.info(f'Reformulated {reformulated} cases out of {passed} cases. Remains {len(cases) - passed} cases to explore.')
 
 
 def main(args):
-    corr_file = args.corr_file if args.corr_file is not None else os.path.splitext(args.error_file)[0] + '.corr'
-    if not pathlib.Path(corr_file).is_file() or len(open(corr_file).readlines()) < 1:
-        logger.info(f'Correction file does not exist or invalid. Created {corr_file} from {args.error_file}.')
-        copyfile(args.error_file, corr_file)
+    corr_file = os.path.splitext(args.error_file)[0] + '.corr'
 
-    logger.info(f'Loading error cases from {corr_file}...')
-    explored_cases = []
-    remained_cases = []
-    reformulated = 0
-    with open(corr_file) as f:
-        for line in f:
-            case = json.loads(line)
-            if 'dirty' in case:
-                explored_cases.append(case)
-            else:
-                remained_cases.append(case)
-            if 'sq' in case:
-                reformulated += 1
-    with open(corr_file, 'w') as f:
-        for case in explored_cases:
-            f.write(json.dumps(case) + '\n')
-        f.flush()
-
-    logger.info(f'Write back {len(explored_cases)} cases.')
-    logger.info(f'{len(explored_cases) + len(remained_cases)} cases loaded.')
-    logger.info(f'{len(explored_cases)} cases explored with {reformulated} reformulated.')
-    logger.info(f'{len(remained_cases)} cases needs to be expored.')
-
-    correct_cases(remained_cases, corr_file)
+    logger.info(f'Loading error cases from {args.error_file}, start from {args.start_index}.')
+    cases = []
+    with open(args.error_file) as f:
+        for i, line in enumerate(f):
+            if i + 1 >= args.start_index:
+                cases.append(json.loads(line))
+    logger.info(f'{len(cases)} cases loaded.')
+    correct_cases(cases, corr_file)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--error_file', type=str, default=None, help='error cases', required=True)
-    parser.add_argument('--corr_file', type=str, default=None, help='corrected error cases.')
-    parser.add_argument('--doc-db', type=str, default=None,
-                        help='Path to Document DB')
+    parser = argparse.ArgumentParser(description='Adapt SQuAD questions for open domain QA')
+    parser.add_argument('--error_file', type=str, default=None, help='error cases file to read from', required=True)
+    parser.add_argument('--start_index', type=int, help='index number to start with (1 based). ', required=True)
 
     args = parser.parse_args()
 
